fdfc84e00cd2c92dd8018dc0abad2aea26be992c
Abhay Bokade <abhaybokade2512@gmail.com>
2021-06-15 18:58:05 +0530

    MyMockTest App with Redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainApp from './MainApp';
+//npm i -g json-server
+//npx json-server --watch db.json --port 3006
+
+function App() {
+  return (
+    <div>
+      <MainApp />
+    </div>
+  )
+}
+
+export default App
